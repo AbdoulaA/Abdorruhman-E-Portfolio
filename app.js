f9f86d8f6df6c5054d146b2a57a69e37f8c77c70
AbdoulaA <206abdoula@gmail.com>
2023-03-12 22:36:18 -0500

    arabic

--- app.js
+++ app.js
@@ -2,18 +2,17 @@
 //
 //
 
-var hideElements = document.getElementsByClassName("hide");
-
-const loading = document.querySelector(".modal__overlay--loading");
-const success = document.querySelector(".modal__overlay--success");
-
 function contact(event) {
   event.preventDefault();
-  
   var hideElements = document.getElementsByClassName("hide");
-  
+
+  var loading = document.querySelector(".modal__overlay--loading");
+  var success = document.querySelector(".modal__overlay--success");
+
+  // var hideElements = document.getElementsByClassName("hide");
+
   loading.classList.add("modal__overlay--visible");
-  
+
   for (var i = 0; i < hideElements.length; i++) {
     hideElements[i].style.display = "none";
   }
@@ -27,7 +26,7 @@ function contact(event) {
     )
     .then(() => {
       loading.classList.remove("modal__overlay--visible");
-      success.classList.add("modal__overlay--visible");
+      document.querySelector(".modal__overlay--success").style.display = "flex";
       document.querySelector(".fa-times").style.color = "#16262e";
     })
     .catch(() => {
@@ -38,49 +37,33 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false
+let isModalOpen = false;
 function toggleModalOn() {
   if (isModalOpen) {
-    isModalOpen = false
-    return document.body.classList.remove("modal__open")
+    isModalOpen = false;
+    return document.body.classList.remove("modal__open");
   }
-  isModalOpen = true
-  document.body.classList += " modal__open"
+  isModalOpen = true;
+  document.body.classList += " modal__open";
 }
 
-// function toggleModalOff() {
-//   document.body.classList.remove("modal__open")
-//   isModalOpen = false
-// }
+function normalAtClose() {
+  var hideElements = document.getElementsByClassName("hide");
 
+  document.querySelector(".modal__overlay--success").style.display = "none"
+  for (var i = 0; i < hideElements.length; i++) {
+    hideElements[i].style.display = "block";
+  }
+  document.querySelector(".fa-times").style.color = "#d6eeff";
+}
 
-// let isModalOpen = false
-// function toggleModalOn() {
-//   document.querySelector(".fa-times").style.color = "#d6eeff";
+function toggleTheme() {
+  document.getElementById("body").classList.toggle("dark");
+}
 
-//   if (isModalOpen) {
-//     isModalOpen = false
-//     return document.body.classList.remove("modal__open")
-//   }
-//   else {
-//     isModalOpen = true
-
-//     document.body.classList += " modal__open"
-//   }
-//   document.querySelector(".modal__overlay").remove("modal__overlay--loading")
-
-// }
-
-// function toggleModalOff() {
-//   for (var i = 0; i < hideElements.length; i++) {
-//     hideElements[i].style.display = "block";
-//   }
-//   toggleModalOn()
-//   document.querySelector(".modal__overlay--success").remove("modal__overlay--visible")
-//   document.querySelector(".fa-times").style.color = "#d6eeff";
-//   // document.querySelector(".modal__overlay").remove("modal__overlay--loading")
-
-//   isModalOpen = false
-  
-// }
-
+function toggleArabic() {
+  document.getElementById("arabic__secondary").classList.toggle("arabic__toggle");
+}
+function toggleEnglish()  {
+  document.getElementById("english__secondary").classList.toggle("english__toggle");
+}
